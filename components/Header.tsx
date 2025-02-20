1cdc966aa37c6d375aefc8daa4066ba93ad1cf35
Xavier Kahn <xakkahn2001@gmail.com>
2025-02-20 16:54:10 -0400

    More header edits

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,28 @@
 "use client";
 
-import { useUser } from "@clerk/nextjs";
+import { ClerkLoaded, SignInButton, useUser, useClerk } from "@clerk/nextjs";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import MobileMenu from "./MobileMenu"; // Import the mobile menu
-import { TrolleyIcon } from "@sanity/icons";
-import { UserIcon } from "@sanity/icons";
+import MobileMenu from "./MobileMenu";
+import { PackageIcon, TrolleyIcon, UserIcon } from "@sanity/icons";
 
 function Header() {
   const { user } = useUser();
+  const { signOut } = useClerk();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleMouseEnter = () => setIsDropdownOpen(true);
+  const handleMouseLeave = () => setIsDropdownOpen(false);
+
+  const dropdownItemClasses =
+    "w-full py-1 px-2 text-xs text-center text-black hover:text-[#27aae1] transition-colors duration-200";
+  const emailDisplayClasses =
+    "w-full py-1 px-2 text-xs text-center text-black cursor-default";
 
   return (
-    <header className="flex flex-col items-center px-6 py-4 bg-white shadow-md w-full">
-      {/* Logo (Visible Only on Small Screens) */}
-      <Image
-        src="/Logo.svg"
-        alt="Logo"
-        width={140}
-        height={70}
-        className="sm:hidden mb-2"
-      />
-
-      {/* Desktop Navigation (Visible Only on Medium and Larger Screens) */}
+    <header className="relative flex flex-col items-center px-6 py-4 bg-white shadow-md w-full">
+      {/* Desktop Navigation */}
       <div className="hidden sm:flex justify-center items-center w-full">
         {/* Left Navigation Links */}
         <nav className="flex items-center gap-6 text-sm font-medium text-black">
@@ -39,7 +40,7 @@ function Header() {
           </Link>
         </nav>
 
-        {/* Logo (Visible Only on Medium and Larger Screens) */}
+        {/* Logo */}
         <Image
           src="/Logo.svg"
           alt="Logo"
@@ -63,22 +64,72 @@ function Header() {
             contact
           </Link>
         </nav>
-        {/* Top Right Icons (Trolley and Profile) */}
-        <div className="absolute top-0 right-0 m-8 flex items-center gap-4">
-          <Link href="/profile">
-            <span>
-              <UserIcon className="w-6 h-6" />
-            </span>
-          </Link>
-          <Link href="/cart">
-            <span>
-              <TrolleyIcon className="w-6 h-6" />
-            </span>
-          </Link>
-        </div>
       </div>
 
-      {/* Mobile Menu (Appears Below Logo on Small Screens) */}
+      {/* Mobile Logo */}
+      <Image
+        src="/Logo.svg"
+        alt="Logo"
+        width={140}
+        height={70}
+        className="sm:hidden mb-2"
+      />
+
+      {/* Top Right Icons */}
+      <div className="hidden sm:flex absolute top-0 right-0 m-8 flex items-center gap-4">
+        <ClerkLoaded>
+          {user ? (
+            // User Icon and Dropdown
+            <div
+              className="relative"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              {/* User Icon */}
+              <div className="cursor-pointer">
+                <UserIcon className="w-6 h-6 hover:text-[#27aae1] transition-colors duration-200" />
+              </div>
+              {/* Dropdown Menu */}
+              {isDropdownOpen && (
+                <div className="absolute right-0 w-auto bg-white border rounded shadow-md flex flex-col">
+                  <div className={emailDisplayClasses}>
+                    <p className="text-xs text-gray-400">Signed in as</p>
+                    <p className="text-xs font-bold">
+                      {user.emailAddresses?.[0]?.emailAddress}
+                    </p>
+                  </div>
+                  <Link href="/orders" className={dropdownItemClasses}>
+                    My Orders
+                  </Link>
+
+                  <button
+                    onClick={() => signOut()}
+                    className={dropdownItemClasses}
+                  >
+                    Sign Out
+                  </button>
+                </div>
+              )}
+            </div>
+          ) : (
+            // Sign In Button
+            <SignInButton mode="modal">
+              <div className="flex flex-col items-center cursor-pointer">
+                <UserIcon className="w-6 h-6 hover:text-[#27aae1] transition-colors duration-200" />
+              </div>
+            </SignInButton>
+          )}
+        </ClerkLoaded>
+
+        {/* Trolley Icon */}
+        <Link href="/cart">
+          <span>
+            <TrolleyIcon className="w-6 h-6 hover:text-[#27aae1] transition-colors duration-200" />
+          </span>
+        </Link>
+      </div>
+
+      {/* Mobile Menu */}
       <MobileMenu />
     </header>
   );
--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -4,9 +4,22 @@ import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
+import { TrolleyIcon, UserIcon } from "@sanity/icons";
+import { ClerkLoaded, SignInButton, useClerk, useUser } from "@clerk/nextjs";
 
 function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { signOut } = useClerk();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleMouseEnter = () => setIsDropdownOpen(true);
+  const handleMouseLeave = () => setIsDropdownOpen(false);
+
+  const dropdownItemClasses =
+    "w-full py-1 px-2 text-xs text-center text-black hover:text-[#27aae1] transition-colors duration-200";
+  const emailDisplayClasses =
+    "w-full py-1 px-2 text-xs text-center text-black cursor-default";
 
   return (
     <div className="sm:hidden flex flex-col items-center w-full">
@@ -41,6 +54,56 @@ function MobileMenu() {
             className="text-black hover:text-[#27aae1] text-lg"
           >
             contact
+          </Link>
+          <ClerkLoaded>
+            {user ? (
+              // User Icon and Dropdown
+              <div
+                className="relative"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                {/* User Icon */}
+                <div className="cursor-pointer">
+                  <UserIcon className="w-6 h-6 hover:text-[#27aae1] transition-colors duration-200" />
+                </div>
+                {/* Dropdown Menu */}
+                {isDropdownOpen && (
+                  <div className="absolute left-1/2 transform -translate-x-1/2 w-auto bg-white border rounded shadow-md flex flex-col">
+                    <div className={emailDisplayClasses}>
+                      <p className="text-xs text-gray-400">Signed in as</p>
+                      <p className="text-xs font-bold">
+                        {user.emailAddresses?.[0]?.emailAddress}
+                      </p>
+                    </div>
+                    <Link href="/orders" className={dropdownItemClasses}>
+                      My Orders
+                    </Link>
+
+                    <button
+                      onClick={() => signOut()}
+                      className={dropdownItemClasses}
+                    >
+                      Sign Out
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              // Sign In Button
+              <SignInButton mode="modal">
+                <div className="flex flex-col items-center cursor-pointer">
+                  <UserIcon className="w-6 h-6 hover:text-[#27aae1] transition-colors duration-200" />
+                </div>
+              </SignInButton>
+            )}
+          </ClerkLoaded>
+
+          {/* Trolley Icon */}
+          <Link href="/cart">
+            <span>
+              <TrolleyIcon className="w-6 h-6 hover:text-[#27aae1] transition-colors duration-200" />
+            </span>
           </Link>
         </nav>
       )}
