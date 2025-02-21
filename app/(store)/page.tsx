966cce4c4713bc81935fafaf7a8ef70fbbe87f2d
Xavier Kahn <xakkahn2001@gmail.com>
2025-02-20 21:55:30 -0400

    edits

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <Button>Click Me</Button>
-    </div>
-  );
+  return <div></div>;
 }
