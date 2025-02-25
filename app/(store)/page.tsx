84aa9ad9ca226971622525a64e126dcd4cc89365
Xavier Kahn <xakkahn2001@gmail.com>
2025-02-25 09:48:52 -0400

    working with sanity

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import Image from "next/image";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home() {
+  const products = await getAllProducts();
+
+  return <div>{/* render all the products*/}</div>;
 }
